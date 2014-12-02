8cb14aaebccc4e794ceb21a89df776451c901d36
Jim Englert <englert.james@gmail.com>
2014-12-01 22:15:23 -0500

    Updates.

--- align.py
+++ align.py
@@ -4,6 +4,7 @@ from os.path import isfile, join
 import re
 from airspeed import CachingFileLoader
 from SimpleCV import Image
+import shutil
 
 def load_cache(contents):
 	loader = CachingFileLoader("html")
@@ -36,23 +37,80 @@ def write_file(file_path, contents):
 	f.write(contents)
 	f.close()
 
-def correct_alignment(si, step_number):
+def step_file_path(si, step_number):
 	cwd = os.getcwd()
-	new_file_path = cwd + "/corrected/" + si.index + "-" + si.name + "-" + si.foot + "-Step" + str(step_number) + ".jpg"
-	img = Image(si.original_file_name) 
+	return cwd + "/corrected/" + si.index + "-" + si.name + "-" + si.foot + "-Step" + str(step_number) + ".jpg"
+
+def scale_down(si, step_number):
+	new_file_path = step_file_path(si, step_number)
+	img = Image(si.step_outputs[len(si.step_outputs) - 1])
+	img = img.resize(h=1024)
+	img.save(new_file_path)
+	return new_file_path
+
+def correct_alignment(si, step_number):
+	new_file_path = step_file_path(si, step_number)
+	img = Image(si.original_file_name) 	
 	if (img.width > img.height):
 		img.rotate(-90, fixed=False).save(new_file_path)
 	else:
 		img.save(new_file_path)
 	return new_file_path
 
+def find_corners(si, step_number):
+	new_file_path = step_file_path(si, step_number)
+	img = Image(si.step_outputs[len(si.step_outputs) - 1])
+	fs = img.findCorners()
+	if (fs != None):
+		fs.draw()
+	img.save(new_file_path)
+	return new_file_path
+
+def find_circles(si, step_number):
+	new_file_path = step_file_path(si, step_number)
+	img = Image(si.step_outputs[len(si.step_outputs) - 1])
+	fs = img.findCircle()
+	if (fs != None):
+		fs.draw()
+	img.save(new_file_path)
+	return new_file_path	
+
+def find_blobs(si, step_number):
+	new_file_path = step_file_path(si, step_number)
+	img = Image(si.step_outputs[len(si.step_outputs) - 1])
+	fs = img.findBlobs()
+	if (fs != None):
+		fs.draw()
+	img.save(new_file_path)
+	return new_file_path
+
+# Strategy is to find blobs that have an edge on the perimeter of the image.  Remove 
+# these blobs from the image.  The blobs also need to be at least 2% of the image.
+def find_and_remove_bottom_blobs(si, step_number):
+	new_file_path = step_file_path(si, step_number)
+	img = Image(si.step_outputs[len(si.step_outputs) - 1])
+	for blob in img.findBlobs():
+		if (blob.area() / img.area() > 0.02):
+			edge_rect = [ point for point in blob.minRect() if point[0] < 10 or point[0] > img.width - 10 or point[1] < 10 or point[1] > img.height - 10]
+			if (len(edge_rect) >= 2):
+				blob.draw()
+	img.save(new_file_path)
+	return new_file_path
+
+def dilate_and_erode(si, step_number):
+	new_file_path = step_file_path(si, step_number)
+	img = Image(si.step_outputs[len(si.step_outputs) - 1])
+	img.erode(1).save(new_file_path)
+	return new_file_path	
+
 # Make the 'corrected' file path
+shutil.rmtree('corrected')
 if not os.path.exists("corrected"):
   os.makedirs("corrected")
 
 sis = sample_images()
 
-steps = [ correct_alignment ]
+steps = [ correct_alignment, scale_down, dilate_and_erode, find_and_remove_bottom_blobs, find_corners ]
 
 for si in sis:
 	for counter, step in enumerate(steps):
